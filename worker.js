708d1affbd86425d1821454e342a18c6913487fd
guest271314 <guest271314@gmail.com>
2023-09-24 21:07:11 -0700

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,32 @@
+// DedicatedWorkerGlobalScope
+let port;
+onmessage = async (e) => {
+  "use strict";
+  if (!port) {
+    [port] = e.ports;
+    port.onmessage = (event) => postMessage(event.data);
+  }
+  const { urls } = e.data;
+  // https://github.com/whatwg/streams/blob/master/transferable-streams-explainer.md
+  const { readable, writable } = new TransformStream();
+  (async (_) => {
+    for await (
+      const _ of (async function* stream() {
+        while (urls.length) {
+          yield (await fetch(urls.shift(), { cache: "no-store" })).body.pipeTo(
+            writable,
+            {
+              preventClose: !!urls.length,
+            },
+          );
+        }
+      })()
+    );
+  })();
+  port.postMessage(
+    {
+      readable,
+    },
+    [readable],
+  );
+};
